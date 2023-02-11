659bd0a1c7cb63879cce701d46b3ffbdc744ba60
TaiL <trongtaile1990@gmail.com>
2023-02-11 23:10:16 +0700

    orphan and zombie process

---
+++ DexTutor/orphan.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int status = 0, w1=0;
+    pid_t q = fork();
+
+    if (q == 0) { //child process
+        sleep(10);
+        printf("I am child having pid is %d\n", getpid());
+        printf("my parent's pid is %d\n", getppid());
+    } else {
+        sleep(3);
+        printf("I am parent having pid is %d\n", getpid());
+        printf("My child pid is %d\n", q);
+    }
+    printf("common\n");
+
+    return 0;
+}
+
